4a20a67317bee181b8bd27d45b1d36c7e96311bd
ferdinandpc <ferdinandpc@gmail.com>
2020-10-27 22:12:03 +0100

    Server upload

---
+++ server/src/main/java/com/fernet/SSE/controller/EventController.java
@@ -0,0 +1,24 @@
+package com.fernet.SSE.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.time.LocalTime;
+
+@RestController
+@RequestMapping("event")
+@CrossOrigin("localhost:4200")
+public class EventController {
+
+	@GetMapping(path = "/receive", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+	public Flux<String> sendEvent() {
+		return Flux.interval(Duration.ofSeconds(5))
+				.map(sequence -> LocalTime.now().toString());
+	}
+
+}
